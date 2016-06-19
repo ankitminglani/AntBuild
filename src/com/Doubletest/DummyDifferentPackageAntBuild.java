301f5f5563b9c7d8c1d2476d3f4cf39e10e06667
preranaminglani <preranaminglani@192.168.0.6>
2016-06-19 14:37:38 +0530

    initial commit

---
+++ src/com/Doubletest/DummyDifferentPackageAntBuild.java
@@ -0,0 +1,9 @@
+package com.Doubletest;
+
+public class DummyDifferentPackageAntBuild {
+
+	public void helloDummy(){
+		System.out.println("I am dummy in another package");
+	}
+	
+}
---
+++ src/com/test/DummyAntBuild.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import com.Doubletest.*;
+
+public class DummyAntBuild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellp Ant again");
+		DummyDifferentPackageAntBuild d= new DummyDifferentPackageAntBuild();
+		d.helloDummy();
+		
+	}
+
+}
